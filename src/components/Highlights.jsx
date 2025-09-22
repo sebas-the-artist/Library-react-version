225329b95ed0f49e21939fb5886f6b7c7f8c402e
sebastian <manijustworkhere@gmail.com>
2025-09-22 10:59:42 -0400

    replacing the copy paste code in the highlights to be dynamic

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Highlights = () => {
     return (
-        <section id="Highlights">
+        <section id="highlights">
             <div className="container">
                 <div className="row">
                     <h2 className="section__title">
@@ -14,7 +14,7 @@ const Highlights = () => {
                             <div className="highlight__img">
                                 <FontAwesomeIcon icon="bolt" />
                             </div>
-                            <h3 className="highlight__subtitle">Easy & Quick</h3>
+                            <h3 className="highlight__subtitle"><span className="purple">Easy</span> & Quick</h3>
                             <p className="highlight__para">
                                 Get access to the book you purchased online <span className="purple">instantly</span>
                             </p>
@@ -23,18 +23,18 @@ const Highlights = () => {
                             <div className="highlight__img">
                                 <FontAwesomeIcon icon="book-open" />
                             </div>
-                            <h3 className="highlight__subtitle">Easy & Quick</h3>
+                            <h3 className="highlight__subtitle"><span className="purple">10,000+</span> books</h3>
                             <p className="highlight__para">
-                                Get access to the book you purchased online <span className="purple">instantly</span>
+                                Library has books in <span className="purple">all</span> your <span className="purple">favorite</span> categories
                             </p>
                         </div>
                         <div className="highlight">
                             <div className="highlight__img">
                                 <FontAwesomeIcon icon="tags" />
                             </div>
-                            <h3 className="highlight__subtitle">Easy & Quick</h3>
+                            <h3 className="highlight__subtitle">Affordable</h3>
                             <p className="highlight__para">
-                                Get access to the book you purchased online <span className="purple">instantly</span>
+                                Get books for as little as <span className="purple">$10</span>
                             </p>
                         </div>
                     </div>
